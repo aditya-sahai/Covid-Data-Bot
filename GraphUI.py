e22bebed783be195deedf0ae071c827c2001d056
Buddy <adityasahai925@gmail.com>
2020-07-10 19:47:08 +0530

    Showing Multiple Countries Total Deaths

--- GraphUI.py
+++ GraphUI.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import pycountry as pyc
 
 
 class GraphUI:
@@ -13,6 +14,42 @@ class GraphUI:
         with open(self.DATAFILENAME, "r") as data_file:
             self.covid_data = json.load(data_file)
 
+    def get_country_iso_codes(self):
+        """Returns a list of the iso codes of the countries entered by the user."""
+
+        number_of_countries = input("\nEnter the number of countries: ").strip()
+
+        if number_of_countries.isdigit():
+            number_of_countries = int(number_of_countries)
+
+            iso_codes = []
+            print()
+
+            for x in range(number_of_countries):
+                country = input("Enter the name of the country: ").strip().lower()
+                iso_code = pyc.countries.get(name=country)
+
+                if iso_code:
+                    iso_codes.append(iso_code.alpha_3)
+
+                else:
+                    print(f"Could not find country with the name '{country}'.")
+
+            return iso_codes
+
+        else:
+            print("Please enter a number.")
+
+    def show_multiple_countries_total_deaths(self):
+        """Asks the user for the countries names and plots a graph."""
+        iso_codes = self.get_country_iso_codes()
+        number_of_days = int(input("\nEnter the number of days: "))
+
+        for iso_code in iso_codes:
+            self.plot_single_country_deaths(iso_code, (0, 0, 0), number_of_days=number_of_days)
+
+        self.format_and_show_graph()
+
     def plot_single_country_deaths(self, country_abbr, color, number_of_days=21):
         """Plots the deaths data of a single country BUT DOES NOT CALL SHOW FUNCTION."""
 
@@ -22,7 +59,8 @@ class GraphUI:
             country_deaths_list.append(int(country_data["total_deaths"]))
             dates_list.append(country_data["date"])
 
-        plt.plot(dates_list, country_deaths_list, color=color)
+        # plt.plot(dates_list, country_deaths_list, color=color)
+        plt.plot(dates_list, country_deaths_list)
 
     def format_and_show_graph(self):
         """Rotates, sets the x and y labels and shows the graph."""
@@ -36,6 +74,8 @@ class GraphUI:
 
 if __name__ == "__main__":
     GraphObj = GraphUI()
-    GraphObj.plot_single_country_deaths("IND", (1, 0, 0), number_of_days=50)
-    GraphObj.plot_single_country_deaths("CHN", (0, 1, 0), number_of_days=50)
-    GraphObj.format_and_show_graph()
+    # GraphObj.get_country_iso_codes()
+    GraphObj.show_multiple_countries_total_deaths()
+    # GraphObj.plot_single_country_deaths("IND", (1, 0, 0), number_of_days=50)
+    # GraphObj.plot_single_country_deaths("CHN", (0, 1, 0), number_of_days=50)
+    # GraphObj.format_and_show_graph()
